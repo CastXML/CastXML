5698edde9f74dafce2265c2dcc23d9f62f36cbc1
Brad King <brad.king@kitware.com>
2014-01-07 10:59:48 -0500

    Add castxml executable
    Load command line arguments through KWSys Encoding.
    Install the executable to 'bin/castxml'.

---
+++ src/castxml.cxx
@@ -0,0 +1,29 @@
+/*
+  Copyright Kitware, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+#include <cxsys/Encoding.hxx>
+
+//----------------------------------------------------------------------------
+int main(int argc, const char* const * argv)
+{
+  cxsys::Encoding::CommandLineArguments args =
+    cxsys::Encoding::CommandLineArguments::Main(argc, argv);
+  argc = args.argc();
+  argv = args.argv();
+  (void)argc;
+  (void)argv;
+  return 0;
+}
