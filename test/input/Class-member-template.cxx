edeba50dd3c12bae48a2b7b5d059184df54b8a62
Brad King <brad.king@kitware.com>
2014-03-06 14:12:50 -0500

    test: Add cases for class member template instantiation
    Cover member templates of both classes and class templates.

---
+++ test/input/Class-member-template.cxx
@@ -0,0 +1,4 @@
+class start {
+  template <typename T> T method(T v) { return v; }
+};
+template int start::method<int>(int); // instantiation
---
+++ test/input/Class-template-member-template.cxx
@@ -0,0 +1,4 @@
+template <typename T> class start {
+  template <typename U> T method(U) { return T(); }
+};
+template int start<int>::method<char>(char); // instantiation
