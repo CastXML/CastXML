5a90589770fd53510981f943844cb488eb58bd91
Brad King <brad.king@kitware.com>
2016-03-17 13:16:42 -0400

    Output: Update `cvs_revision` number to indicate output change
    Our recent changes to argument `default=""` and variable `init=""`
    content mean tools need to interpret the values differently.
    Update the file format revision number so tools can distinguish
    the new content from that produced by older CastXML versions.
    
    GitHub-Issue: CastXML/CastXML#51
    Suggested-by: Michka Popoff <michkapopoff@gmail.com>

--- src/Output.cxx
+++ src/Output.cxx
@@ -2069,7 +2069,7 @@ void ASTVisitor::HandleTranslationUnit(clang::TranslationUnitDecl const* tu)
   // Start dump with gccxml-compatible format.
   this->OS <<
     "<?xml version=\"1.0\"?>\n"
-    "<GCC_XML version=\"0.9.0\" cvs_revision=\"1.136\">\n"
+    "<GCC_XML version=\"0.9.0\" cvs_revision=\"1.137\">\n"
     ;
 
   // Dump the complete nodes.
