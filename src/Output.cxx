CastXML/CastXML

commit 917c646ed9e29ca57fd53791a9b3f5f97e86d8b9

Output: Generalize exclusion of internal `__castxml` builtins
Generalize exclusion of our internal `__castxml__float128` structure to
exclude all translation-unit-level named declarations whose names
contain `__castxml`.  This will support other future internal builtins.

src/Output.cxx
@@ -809,15 +809,21 @@ void ASTVisitor::AddDeclContextMembers(clang::DeclContext const* dc,
       continue;
     }
 
+    // Skip declarations that we use internally as builtins.
+    if (isTranslationUnit) {
+      if (clang::NamedDecl const* nd = clang::dyn_cast<clang::NamedDecl>(d)) {
+        if (nd->getName().find("__castxml") != std::string::npos) {
+          continue;
+        }
+      }
+    }
+
     // Ignore certain members.
     switch (d->getKind()) {
     case clang::Decl::CXXRecord: {
       clang::CXXRecordDecl const* rd =
         static_cast<clang::CXXRecordDecl const*>(d);
       if (rd->isInjectedClassName()) {
-        continue;
-      }
-      if (isTranslationUnit && rd->getName() == "__castxml__float128") {
         continue;
       }
     } break;
@@ -853,12 +859,6 @@ void ASTVisitor::AddDeclContextMembers(clang::DeclContext const* dc,
         static_cast<clang::NamespaceDecl const*>(d);
       if (nd->isInline()) {
         this->AddDeclContextMembers(nd, emitted);
-        continue;
-      }
-    } break;
-    case clang::Decl::Record: {
-      clang::RecordDecl const* rd = static_cast<clang::RecordDecl const*>(d);
-      if (isTranslationUnit && rd->getName() == "__castxml__float128") {
         continue;
       }
     } break;

commit d5934bd08651dbda95a65ccadcc5f39637d7bc59

Output: Fix assertion failure on translation-unit-level operators
Refactoring in commit 917c646e (Output: Generalize exclusion of internal
`__castxml` builtins, 2016-04-18) introduced an unconditional call to
`clang::NamedDecl::getName`, but that method asserts that the name is
a valid identifier.  Instead call `getIdentifier` ourselves and check
the name only if it is a valid identifier.

GitHub-Issue: #62

src/Output.cxx
@@ -812,8 +812,10 @@ void ASTVisitor::AddDeclContextMembers(clang::DeclContext const* dc,
     // Skip declarations that we use internally as builtins.
     if (isTranslationUnit) {
       if (clang::NamedDecl const* nd = clang::dyn_cast<clang::NamedDecl>(d)) {
-        if (nd->getName().find("__castxml") != std::string::npos) {
-          continue;
+        if (clang::IdentifierInfo const* ii = nd->getIdentifier()) {
+          if (ii->getName().find("__castxml") != std::string::npos) {
+            continue;
+          }
         }
       }
     }

test/CMakeLists.txt
@@ -400,6 +400,10 @@ castxml_test_gccxml(using-declaration-start)
 castxml_test_gccxml(using-directive-ns)
 castxml_test_gccxml(using-directive-start)
 
+set(castxml_test_gccxml_custom_start "")
+castxml_test_gccxml(nostart)
+unset(castxml_test_gccxml_custom_start)
+
 if(";${LLVM_TARGETS_TO_BUILD};" MATCHES ";X86;")
   set(castxml_test_gccxml_extra_arguments -target i386-pc-windows-msvc)
   castxml_test_gccxml(Function-calling-convention-ms)

test/input/nostart.cxx
@@ -1,0 +1,2 @@
+class A;
+A operator+(A,A);