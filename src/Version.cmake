d1523f24769dd28e06370cf01836a380bf8848a1
Brad King <brad.king@kitware.com>
2022-12-14 12:39:06 -0500

    CastXML 0.5.1

--- src/Version.cmake
+++ src/Version.cmake
@@ -1,7 +1,7 @@
 # CastXML version number components.
 set(CastXML_VERSION_MAJOR 0)
 set(CastXML_VERSION_MINOR 5)
-set(CastXML_VERSION_PATCH 0)
+set(CastXML_VERSION_PATCH 1)
 #set(CastXML_VERSION_RC 0)
 set(CastXML_VERSION_IS_DIRTY 0)
 
