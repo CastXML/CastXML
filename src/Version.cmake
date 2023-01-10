3b9b693ccc9be5a387256c36c46e1d9152b76a13
Brad King <brad.king@kitware.com>
2023-01-10 10:08:10 -0500

    Version: Update development date

--- src/Version.cmake
+++ src/Version.cmake
@@ -1,7 +1,7 @@
 # CastXML version number components.
 set(CastXML_VERSION_MAJOR 0)
 set(CastXML_VERSION_MINOR 5)
-set(CastXML_VERSION_PATCH 20221215)
+set(CastXML_VERSION_PATCH 20230110)
 #set(CastXML_VERSION_RC 0)
 set(CastXML_VERSION_IS_DIRTY 0)
 
